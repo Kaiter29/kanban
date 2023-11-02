9249214703673aeb0f4717c40f973697b0f073c8
Alexsander Moura Gonçalves <alexmgoncalves09@gmail.com>
2023-11-02 13:30:30 -0300

    Adicionado projeto Kanban

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="css/styles.css" />
+  </head>
+  <body>
+    <header>
+      <div class="info-container">
+          <h1>Kanban</h1>
+      </div>
+      <div>
+        <button class="add-card" type="button" onclick="AbreAddCard();">Adicionar Cartão</button>
+      </div>
+      <form class="input-container" id="input-container">
+        <div class="row">
+          <div>
+            <h2>#Adicionar Cartão</h2>
+          </div>
+          <div>
+            <button class="close-card" type="button" onclick="fechaAddCard();">X</button>
+          </div>
+        </div>
+        <div class="fields">
+          <input id="title" placeholder="Título" /><br>
+          <input id="description" placeholder="Descrição" />
+        </div>
+        <div>
+          <button class="new-card" type="submit" id="submit-button">Criar</button>
+        </div>
+      </form>
+    </header>
+
+    <section class="column-container">
+      <div class="task-column" id="card">
+        <h3>Análise</h3>
+        <hr class="custom-hr" />
+        <div class="task-list"></div>
+      </div>
+
+      <div class="task-column" id="alinhamento">
+        <h3>Alinhamento</h3>
+        <hr class="custom-hr" />
+        <div class="task-list"></div>
+      </div>
+
+      <div class="task-column" id="desenvolvimento">
+        <h3>Desenvolvimento</h3>
+        <hr class="custom-hr" />
+        <div class="task-list"></div>
+      </div>
+
+      <div class="task-column" id="codereview">
+        <h3>Code Review</h3>
+        <hr class="custom-hr" />
+        <div class="task-list"></div>
+      </div>
+    </section>
+
+    <div class="error-container"></div>
+
+    <script src="js/app.js"></script>
+  </body>
+</html>
---
+++ js/app.js
@@ -0,0 +1,147 @@
+const taskLists = document.querySelectorAll(".task-list");
+const cardTasks = document.querySelector("#card .task-list");
+const titleInput = document.querySelector("#title");
+const descriptionInput = document.querySelector("#description");
+const submitButton = document.querySelector("#submit-button");
+const errorContainer = document.querySelector(".error-container");
+
+let tasks = [];
+
+function abreAddColuna() {
+    document.getElementById('input-container').style.display = 'block';
+}
+
+function fechaAddColuna() {
+    document.getElementById('input-container').style.display = 'none';
+}
+
+function AbreAddCard() {
+    document.getElementById('input-container').style.display = 'block';
+}
+
+function fechaAddCard() {
+    document.getElementById('input-container').style.display = 'none';
+}
+
+taskLists.forEach((taskList) => {
+  taskList.addEventListener("dragover", dragOver);
+  taskList.addEventListener("drop", dragDrop);
+});
+
+function createTask(taskId, title, description) {
+  const taskCard = document.createElement("div");
+  const taskHeader = document.createElement("div");
+  const taskTitle = document.createElement("p");
+  const taskDescriptionContainer = document.createElement("div");
+  const taskDescription = document.createElement("p");
+  const deleteIcon = document.createElement("p");
+
+  taskCard.classList.add("task-container");
+  taskHeader.classList.add("task-header");
+  taskDescriptionContainer.classList.add("task-description-container");
+
+  taskTitle.textContent = title;
+  taskDescription.textContent = description;
+  deleteIcon.textContent = "Excluir";
+
+  taskCard.setAttribute("draggable", true);
+  taskCard.setAttribute("task-id", taskId);
+
+  taskCard.addEventListener("dragstart", dragStart);
+  deleteIcon.addEventListener("click", deleteTask);
+
+  taskHeader.append(taskTitle, deleteIcon);
+  taskDescriptionContainer.append(taskDescription);
+  taskCard.append(taskHeader, taskDescriptionContainer);
+  cardTasks.append(taskCard);
+}
+
+function addColor(column) {
+  let color;
+  switch (column) {
+    case "analise":
+      color = "rgb(96, 96, 192)";
+      break;
+    case "alinhamento":
+      color = "rgb(83, 156, 174)";
+      break;
+    case "desenvolvimento":
+      color = "rgb(224, 165, 116)";
+      break;
+    case "codereview":
+      color = "rgb(222, 208, 130)";
+      break;
+    default:
+      color = "rgb(232, 232, 232)";
+  }
+  return color;
+}
+
+function addTasks() {
+  tasks.forEach((task) => createTask(task.id, task.title, task.description));
+}
+
+addTasks();
+
+let elementBeingDragged;
+
+function dragStart() {
+  elementBeingDragged = this;
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragDrop() {
+  const columnId = this.parentNode.id;
+  elementBeingDragged.firstChild.style.backgroundColor = addColor(columnId);
+  this.append(elementBeingDragged);
+}
+
+function showError(message) {
+  const errorMessage = document.createElement("p");
+  errorMessage.textContent = message;
+  errorMessage.classList.add("error-message");
+  errorContainer.append(errorMessage);
+
+  setTimeout(() => {
+    errorContainer.textContent = "";
+  }, 2000);
+}
+
+function addTask(e) {
+  e.preventDefault();
+  const filteredTitles = tasks.filter((task) => {
+    return task.title === titleInput.value;
+  });
+
+  if (!filteredTitles.length) {
+    const newId = tasks.length;
+    tasks.push({
+      id: newId,
+      title: titleInput.value,
+      description: descriptionInput.value
+    });
+    createTask(newId, titleInput.value, descriptionInput.value);
+    titleInput.value = "";
+    descriptionInput.value = "";
+  } else {
+    showError("Título em uso!");
+  }
+}
+submitButton.addEventListener("click", addTask);
+
+function deleteTask() {
+  const headerTitle = this.parentNode.firstChild.textContent;
+
+  const filteredTasks = tasks.filter((task) => {
+    return task.title === headerTitle;
+  });
+
+  tasks = tasks.filter((task) => {
+    return task !== filteredTasks[0];
+  });
+
+  this.parentNode.parentNode.remove();
+}
